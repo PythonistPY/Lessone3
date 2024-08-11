1bd1f9680f585825662b4e4963c137e1d498b58f
Kimi Ktoiani <kimo.tiens@gmail.com>
2024-08-11 19:52:06 +0400

    update pic jpg

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Simple")
-icon = pygame.image.load("")
+icon = pygame.image.load("img/download.JPG")
 
 running = True
 while running:
