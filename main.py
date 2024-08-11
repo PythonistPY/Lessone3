95a530e16c25b50372617e7d4e42b893edc575fc
Kimi Ktoiani <kimo.tiens@gmail.com>
2024-08-11 19:06:57 +0400

    add new const

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Simple")
+icon = pygame.image.load("")
+
 running = True
 while running:
     pass
-
 
 pygame.quit()
