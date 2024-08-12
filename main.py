85a84c04b6abae2fc8d222432e43fc68b3f3ebc2
Kimi Ktoiani <kimo.tiens@gmail.com>
2024-08-12 15:06:29 +0400

    add all const

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pygame
+import random
+
+
 
 pygame.init()
 
@@ -9,6 +12,19 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Simple")
 icon = pygame.image.load("img/download.JPG")
+pygame.display.set_icon(icon)
+
+
+target_img = pygame.image.load("img/target.png")
+target_width = 100
+target_height = 100
+
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+         )
+
 
 running = True
 while running:
